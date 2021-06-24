c31afa67412b0639a2e047ecf7d97aaf73b6e7e3
Kevin Baker <kenovics@gmail.com>
2021-06-24 12:28:02 -0500

    workinprogress

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+let lyrics;
+let wordList = [];
+let stroke_color;
+let fill_color;
+let mySound;
 function preload() {
+	//loads our assets
+	lyrics = loadStrings('./assets/lyrics.txt');
+	soundFormats('mp3', 'ogg');
+	mysound = loadSound('assets/usblues.mp3');
+	console.log(lyrics);
 }
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(250);
+	createCanvas(windowWidth, windowHeight);
+	background(250);
+	strokeWeight(3);
+	stroke_color = color('pink');
+	fill_color = color('white');
+	textSize(40);
+	lyrics.forEach((line) => {
+			wordList.push(...line.split(' '));
+			});
+	console.log(wordList);
+	mysound.play();
 }
-
 function draw() {
+	fill(fill_color);
+	stroke(stroke_color);
+	let rateNum = 3;
+	if (frameCount % rateNum == 0) {
+		let currWord = wordList[frameCount/rateNum % wordList.length];
+		text(currWord, mouseX, mouseY);
+	}
 }
-
